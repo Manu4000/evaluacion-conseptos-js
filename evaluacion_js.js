9b440434222e0a04dc41512b308656b0beaf511e
manuTorres <manolitotorres.2020@gmail.com>
2023-02-07 19:55:16 -0300

    hice el ejercicio b

--- evaluacion_js.js
+++ evaluacion_js.js
@@ -1,2 +1,8 @@
-var nombre = prompt("Por favor ingrese su nombre");
-alert("Bienvenido " + nombre + "!");
+//ejercicio 3 a)
+//var nombre = prompt("Por favor ingrese su nombre"); alert("Bienvenido " + nombre + "!");
+
+//ejercicio 3 b)
+var num1 = parseInt(prompt("Por favor ingrese el primer número"));
+var num2 = parseInt(prompt("Por favor ingrese el segundo número"));
+var suma = num1 + num2;
+alert("La suma de " + num1 + " y " + num2 + " es igual a " + suma + ".");
