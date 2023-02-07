50f09d065e56ddc17009aa710945717b42e18d2d
manuTorres <manolitotorres.2020@gmail.com>
2023-02-07 20:05:17 -0300

    hice el ejercicio 3 c

--- evaluacion_js.js
+++ evaluacion_js.js
@@ -10,8 +10,7 @@ alert("La suma de " + num1 + " y " + num2 + " es igual a " + suma + ".");*/
 
 
 //ejercicio 3 c)
-/*var num1 = parseInt(prompt("Por favor ingrese el primer número"));
+/* var num1 = parseInt(prompt("Por favor ingrese el primer número"));
 var num2 = parseInt(prompt("Por favor ingrese el segundo número"));
 var mayor = (num1 > num2) ? num1 : num2;
-alert("El número mayor entre " + num1 + " y " + num2 + " es " + mayor + ".");/*
-
+alert("El número mayor entre " + num1 + " y " + num2 + " es " + mayor + "."); */
