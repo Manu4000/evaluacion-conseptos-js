318387644e4001949d226bb446f4da6680881da3
manuTorres <manolitotorres.2020@gmail.com>
2023-02-07 20:02:40 -0300

    hice el ejercicio 3 c

--- evaluacion_js.js
+++ evaluacion_js.js
@@ -1,8 +1,17 @@
 //ejercicio 3 a)
-//var nombre = prompt("Por favor ingrese su nombre"); alert("Bienvenido " + nombre + "!");
+/*var nombre = prompt("Por favor ingrese su nombre"); 
+alert("Bienvenido " + nombre + "!");*/
 
 //ejercicio 3 b)
-var num1 = parseInt(prompt("Por favor ingrese el primer número"));
+/* var num1 = parseInt(prompt("Por favor ingrese el primer número"));
 var num2 = parseInt(prompt("Por favor ingrese el segundo número"));
 var suma = num1 + num2;
-alert("La suma de " + num1 + " y " + num2 + " es igual a " + suma + ".");
+alert("La suma de " + num1 + " y " + num2 + " es igual a " + suma + ".");*/
+
+
+//ejercicio 3 c)
+/*var num1 = parseInt(prompt("Por favor ingrese el primer número"));
+var num2 = parseInt(prompt("Por favor ingrese el segundo número"));
+var mayor = (num1 > num2) ? num1 : num2;
+alert("El número mayor entre " + num1 + " y " + num2 + " es " + mayor + ".");/*
+
