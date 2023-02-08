248329fda45ffdff04d08370477ffbdb16c7dcc4
manuTorres <manolitotorres.2020@gmail.com>
2023-02-07 21:03:49 -0300

    hice el ejercicio 3 f

--- evaluacion_js.js
+++ evaluacion_js.js
@@ -40,9 +40,27 @@ numeros.sort(function(a, b){return b-a});
 alert("Los números ordenados de mayor a menor son: " + numeros);*/
 
 //ejercicio 3 f)
+//f) Pida al usuario su peso en kg y su altura en metros y muestre su índice de masa corporal (IMC): Peso (en Kg) / Altura (en metros) al cuadrado.
 /*var peso = parseInt(prompt("Ingrese su peso en kg:"));
 var altura = parseInt(prompt("Ingrese su altura en metros:"));
 
 var imc = peso / (altura * altura);
 
 alert("Su índice de masa corporal (IMC) es: " + imc);*/
+
+//Si el número esta por debajo de 18.5 mostrá la leyenda "Bajo peso"Si el número esta entre 18.6 y 24.9  mostrá la leyenda "Normal" Si el número esta entre 25 y 29.9  mostrá la leyenda "Sobrepeso" Si el número esmayor a 30 mostrá la leyenda "Obesidad"
+
+var peso = parseFloat(prompt("Ingrese su peso en kg:"));
+var altura = parseFloat(prompt("Ingrese su altura en metros:"));
+var imc = peso / (altura * altura);
+
+if (imc < 18.6) {
+  alert("Su índice de masa corporal es " + imc + " y está en Bajo peso");
+} else if (imc >= 18.6 && imc <= 24.9) {
+  alert("Su índice de masa corporal es " + imc + " y está en Normal");
+} else if (imc >= 25 && imc <= 29.9) {
+  alert("Su índice de masa corporal es " + imc + " y está en Sobrepeso");
+} else {
+  alert("Su índice de masa corporal es " + imc + " y está en Obesidad");
+}
+
